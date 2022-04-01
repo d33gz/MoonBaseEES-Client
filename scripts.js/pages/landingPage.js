ef28e676c5e62a2a76cffde573bb10736b7ac952
Hendo1349 <hendo1349@gmail.com>
2022-04-01 16:26:58 -0400

    login page done

--- scripts.js/pages/landingPage.js
+++ scripts.js/pages/landingPage.js
@@ -1,12 +1,18 @@
 const contentBox = document.getElementById("content-box");
 
 loadPage = () => {
-  contentBox.innerHTML = (
-    <><div id="landing-box">
-      <h1>Moon Base</h1>
-      <h2>Employee Expense System</h2>
-    </div><button name="enter" type="submit">Login</button></>
-  )
+  console.log("called")
+  // contentBox.innerHTML = (
+  //   <>
+  //   <div id="landing-box">
+  //     <h1>Moon Base</h1>
+  //     <h2>Employee Expense System</h2>
+  //   </div>
+  //   <div id="button-container">
+  //     <a href="loginPage.html"><button id="enter-button" name="login" type="submit">Enter</button></a>
+  //   </div>
+  // </>
+  // )
 };
 
-loadPage();
+document.onload = loadPage();
